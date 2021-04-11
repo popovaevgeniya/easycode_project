7b46c5dd44e1262ad7676493b33fe0a2aa9567a9
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2021-04-11 17:43:57 +0300

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,84 @@
+const path = require('path'),
+  HtmlWebpackPlugin = require('html-webpack-plugin'),
+  {CleanWebpackPlugin} = require('clean-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin')
+  //HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
+
+// const fs = require('fs');
+// const PATHS = {
+//   src: path.join(__dirname, '../src'),
+//   dist: path.join(__dirname, '../dist')
+// }
+// const PAGES_DIR = `${PATHS.src}/pug/pages/`;
+// const PAGES = fs.readdirSync(PAGES_DIR).filter(fileName => fileName.endsWith('.pug'));
+
+module.exports = {
+  mode: 'development',
+  context: path.resolve(__dirname, 'src'),
+
+  entry: './index.js',
+  output: {
+    filename: "[name].[fullhash].js",
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 9000,
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.pug',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin(),
+    //new HtmlWebpackPugPlugin(),
+    // ...PAGES.map(page => new HtmlWebpackPlugin({
+    //   template: `${PAGES_DIR}/${page}`,
+    //   filename: `./${page.replace(/\.pug/,'.html')}`
+    // }))
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', { targets: "defaults" }]
+            ],
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
+      },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      },
+      {
+        test: /\.(jpg|png|svg)$/,
+        loader: 'file-loader'
+      },
+    ],
+  },
+}
