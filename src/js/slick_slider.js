b7696a3a98354ddb56305ebca8d583e3294e7513
Evgeniya Popova <evgeniya.popova@wdg.com.ua>
2021-04-25 23:00:58 +0300

    commit

--- src/js/slick_slider.js
+++ src/js/slick_slider.js
@@ -1,5 +1,11 @@
+import 'slick-carousel';
+// import 'slick-carousel/slick/slick.css';
+// import 'slick-carousel/slick/slick-theme.css';
+
 function makeSlider(){
-    document.getElementsByClassName('reviewsSlider')[0].slick();
+    document.getElementsByClassName('reviewsSlider')[0].slick({
+        infinite: false
+    });
 };
 
 makeSlider();
